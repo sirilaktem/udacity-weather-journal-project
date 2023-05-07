c8b3f03166854664f3e0da7317655b6020a1afc9
sirilaktem <sirilaktem@outlook.com>
2023-05-06 21:20:42 -0700

    fix: Set up server environment

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 // Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
 // Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
 /* Middleware*/
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static('website'));
 
 // Spin up the server
+const port = 8000;
+
 // Callback to debug
+function listening(){
+    console.log(`running on localhost:${port}`);
+}
+app.listen(port, listening);
 
 // Initialize all route with a callback function
 
